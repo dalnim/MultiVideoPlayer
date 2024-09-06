2d0f8d407d2ae45ae3d8a3cbbad3a5c18d3b64ad
dalnim <dalnimbest@gmail.com>
2024-09-06 19:51:16 +0900

    init

---
+++ language.js
@@ -0,0 +1,184 @@
+const jsonEnglish = {
+  "hands__nav": "Hands-Free",
+  "video__nav": "Controll",
+  "shuffle__nav": "Playlist",
+  "gallery__nav": "Screenshots",
+  "feature__nav": "Features",
+  "reviews__nav": "Reviews",
+
+
+  "hero_title": "One video? No!!!",
+  "watch__title": "Mute for Safety!",
+  "watch__desc": "One mistake can ruin everything. Auto-stop when switching to background.",
+  "playback__title": "Multi-Screen",
+  "playback__desc": "Watch up to 4 videos simultaneously on phones. Up to 9 videos on tablets.",
+  "hands_free_title": "Hands-Free",
+  "hands_free_desc": "Infinite repeat + AB repeat + Auto random play",
+  "video_control_title": "Control All or Individual Videos",
+  "video_control_desc": "Play/Pause/Mute simultaneously. Adjust volume for individual videos.",
+  "shuffle_title": "Playlist",
+  "shuffle_desc": "Create playlists by your favorite genres. You can also select playlists for random play.",
+  "awesome_sc": "Awesome Screenshots",
+  "feature_title": "Features",
+  "feature_desc": "",
+  "feature_title_one": "Infinite Repeat",
+  "feature_desc_one": "When playback ends, it starts again from the beginning. AB section playback is also repeated infinitely.",
+  "feature_title_two": "Mute by Default",
+  "feature_desc_two": "Mute by default when first playing. Auto-stop when the app is switched to background.",
+  "feature_title_three": "Multi-Screen Playback",
+  "feature_desc_three": "Enjoy watching on multiple screens simultaneously.",
+  "feature_title_four": "AB Repeat",
+  "feature_desc_four": "If the video is long, create an AB repeat section for your favorite scenes.",
+  "feature_title_five": "Random Play + Playlist",
+  "feature_desc_five": "Watching the same video can get boring. Enjoy continuous new videos with random play.",
+  "feature_title_six": "Time-Shifted Playback",
+  "feature_desc_six": "Experience something new with simultaneous playback or time-shifted playback on multiple screens of the same video.",
+  "testi_title": "Loved By Our Customers",
+  "testi_desc_one": "This is app is quite unique. I don’t remember any app like this that allow you to watch multiple videos at the same time. Must try!",
+  "testi_author_one": "Sarah L.",
+  "testi_desc_two": "Let’s you see 2-4 videos at the same time and lower sound on each individual video thank you",
+  "testi_author_two": "Emily R.",
+  "testi_desc_three": "It is good and convenient app! One thing that I would like to see here is adding videos from different sources like YouTube and others! But for now the video works well.",
+  "testi_author_three": "John M.",
+  "testi_desc_four": "Amazing app! Finally found something that actually works for this. A suggestion: being able to control the volume level between videos! I have one video that might be too loud and want to reduce the volume.",
+  "testi_author_four": "Jessica T.",
+  "testi_desc_five": "The AB repeat function is perfect for my needs. Highly recommend this app for anyone who loves to loop videos.",
+  "testi_author_five": "Michael K.",
+  "testi_desc_six": "I love the shuffle playlist feature. It keeps my watching experience fresh and exciting.",
+  "testi_author_six": "David B.",
+
+  "available__title": "Unsupported Features",
+  "available__desc": "The following features will be supported in the future.",
+  "available__desc__one": "No Web Browser",
+  "available__desc__two": "No YouTube",
+  "available__desc__three": "Not Streaming Yet",
+  "available__desc__four": "Only Pre DownloadedVideos",
+
+  "app__title": "Join community",
+  "app__title__desc": "Join our community on Reddit and Discord! Come hang out, share your experiences, and have fun together!",
+
+  "maill__desc": "araonesoft@gmail.com",
+  "copyright__desc": "Copyright © 2024 AraOneSoft.co. All Rights Reserved.",
+
+
+
+}
+
+const jsonKorean = {
+  "hands__nav": "핸즈프리",
+  "video__nav": "개별조작",
+  "shuffle__nav": "랜덤플레이",
+  "gallery__nav": "멀티화면",
+  "feature__nav": "특징",
+  "reviews__nav": "리뷰",
+
+  "hero_title": "하나로는 부족해!!!",
+  "watch__title": "안전제일! 기본무음",
+  "watch__desc": "재생시작시 갑자기 큰소리가 나면 민망하죠? 그래서 기본무음입니다. 백그라운드 전환시에도 자동 재생 중지되구요.",
+  "playback__title": "멀티 스크린은 기본",
+  "playback__desc": "1~4개 비디오. 패드는 최대 9개까지",
+  "hands_free_title": "핸즈 프리",
+  "hands_free_desc": "자유로운 양손 = 무한 반복 + AB반복 + 랜덤 플레이",
+  "video_control_title": "전체 또는 개별 비디오 조작",
+  "video_control_desc": "동시에 재생/정지/음소거 가능. 개별 비디오의 음량 조절 가능",
+  "shuffle_title": "재생목록",
+  "shuffle_desc": "좋아하는 장르별로 재생 목록을 만드세요. 재생목록들을 선택하여 랜덤 플레이도 가능합니다.",
+  "awesome_sc": "멀티 화면",
+  "feature_title": "특징",
+  "feature_desc": "",
+  "feature_title_one": "무한 반복",
+  "feature_desc_one": "재생이 끝나면 처음부터 다시 재생. AB구간도 무한 반복",
+  "feature_title_two": "기본 무음",
+  "feature_desc_two": "첫 재생시 기본 무음. 앱이 백그라운드로 전환시 자동 멈춤",
+  "feature_title_three": "멀티 스크린 재생",
+  "feature_desc_three": "여러 화면에서 동시에 감상",
+  "feature_title_four": "AB 반복",
+  "feature_desc_four": "비디오가 길면, 좋아하는 장면을 AB 반복구간으로 보세요.",
+  "feature_title_five": "랜덤 플레이",
+  "feature_desc_five": "같은 비디오만 보면 지겹습니다. 랜덤하게 계속 새로운 비디오들을 즐기세요.",
+  "feature_title_six": "시간차 재생",
+  "feature_desc_six": "같은 비디오를 여러 화면에서 시간차 재생으로 즐기세요.",
+
+  "testi_title": "고객 리뷰",
+  "testi_desc_one": "비디오를 동시에 재생도 하고 따로도 되고 너무 유용해서 잘 쓰구있답니다. 최근에 받은 플레이어 어플중에 단연코 최고 인것같습니다~!!",
+  "testi_author_one": "김**",
+  "testi_desc_two": "모든 비디오의 AB 반복 기능이 마음에 드네요. 적극 추천합니다.",
+  "testi_author_two": "박**.",
+  "testi_desc_three": "재미있는 아이디어입니다. 두 영상을 서로 비교해 볼 때 유용해요. 랜덤 플레이와 재생 동영상 저장등 곳곳에 숨은 기능이 꽤 많네요.",
+  "testi_author_three": "이**",
+  "testi_desc_four": "이런 앱이 나오면 좋겠다는 막연한 생각은 있었는데 막상 나오니 새롭네요 여러개 영상을 비교하면서 보고 싶을 때 유용합니다 잘 활용하면 편집할 때도 편할 것 같습니다.",
+  "testi_author_four": "조**",
+  "testi_desc_five": "핸드폰에서도 4개영상을 한번에 재생 할 수 있다니 엄청 좋습니다 ㅜ 그리고 사용법이 까다롭지 않고 패드에선 더 재생도 가능하니 엄청 유용하게 사용중입니다 !! 좋은 어플 만들어주셔 감사합니당 ㅎㅎ",
+  "testi_author_five": "최**",
+  "testi_desc_six": "앱으로 멀티 영상을 볼 수 있는 기능이 처음인 것 같은데 여러가지를 동시에 볼 수 있어 유용하고 편하네요",
+  "testi_author_six": "김**",
+
+  "available__title": "미지원 기능",
+  "available__desc": "아래 기능은 향후에 지원될 예정입니다.",
+  "available__desc__one": "웹 브라우저 기능",
+  "available__desc__two": "유튜브 지원",
+  "available__desc__three": "스트리밍 기능",
+  "available__desc__four": "가지고 있는 비디오만 가능",
+
+  "app__title": "커뮤니티",
+  "app__title__desc": "궁금한점이나 비디오를 구하는법을 모르면 레딧이나 디스코드로 오셔서 물어보세요.",
+
+  "maill__desc": "araonesoft@gmail.com",
+  "copyright__desc": "저작권 © 2024 아라원소프트. 모든 권리 보유.",
+
+}
+
+// Object to store image URLs for each language
+const imageUrls = {
+  en: {
+    shuffleThumbnail: "assets/images/home/auto-playlist-mobail.webp",
+    shuffleThumbnailResponsive: "assets/images/home/auto-playlist-mobail.webp",
+    image2: "path-to-english-image2.jpg"
+  },
+  ko: {
+    shuffleThumbnail: "assets/images/home/auto-playlist-mobail-korean.webp",
+    shuffleThumbnailResponsive: "assets/images/home/auto-playlist-mobail-korean.webp",
+    image2: "path-to-korean-image2.jpg"
+  }
+};
+
+ 
+// function for content update based on selected language
+function updateLanguage(language) {
+  if (language == 'en') {
+    for (let key in jsonEnglish) {
+      document.querySelector('.' + key).textContent = jsonEnglish[key];
+    }
+  } else if (language == 'ko') {
+    for (let key in jsonKorean) {
+      document.querySelector('.' + key).textContent = jsonKorean[key];
+    }
+  }
+  // Update multiple image sources
+  for (let imageId in imageUrls[language]) {
+    document.querySelector('#' + imageId).src = imageUrls[language][imageId];
+    // Save each image URL to localStorage
+    localStorage.setItem(imageId + 'Src', imageUrls[language][imageId]);
+  }
+}
+
+// set language on change and update the localization
+document.querySelector("#language").addEventListener('change', function () {
+  const selectedLanguage = this.value;
+  localStorage.setItem('selectedLanguage', selectedLanguage);
+  updateLanguage(selectedLanguage);
+  console.log('selected lang', selectedLanguage);
+})
+// on page load, set the selected language from local storage
+document.addEventListener('DOMContentLoaded', function () {
+  const storedLanguage = localStorage.getItem('selectedLanguage') || 'en';
+  document.querySelector("#language").value = storedLanguage;
+  updateLanguage(storedLanguage);
+  console.log('stored lang', storedLanguage);
+
+  // Load image sources from localStorage
+  for (let imageId in imageUrls[storedLanguage]) {
+    const storedImageSrc = localStorage.getItem(imageId + 'Src') || imageUrls[storedLanguage][imageId];
+    document.querySelector('#' + imageId).src = storedImageSrc;
+  }
+})
